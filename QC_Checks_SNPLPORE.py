58113d80e3335394797eed5551b30a15dc917f4a
krsherrill <kirk_Sherrill@nps.gov>
2024-07-23 11:05:47 -0600

    Update QC_Checks_SNPLPORE.py
    Adding log file configuration

--- QC_Checks_SNPLPORE.py
+++ QC_Checks_SNPLPORE.py
@@ -9,7 +9,10 @@ import glob, os, sys
 import traceback
 import QC_Checks as qc
 import generalDM as dm
+import logging
+import log_config
 
+logger = logging.getLogger(__name__)
 
 class qcProtcol_SNPLPORE:
 
@@ -67,17 +70,18 @@ class qcProtcol_SNPLPORE:
         :param dmInstance: data management instance which will have the logfile name
 
 
-        :return: Pushed
+        :return:
         """
+
         try:
             if queryName_LU == "qa_a102_Unverified_Events":
-                outFun = qcProtcol_SNPLPORE.qa_a102_Unverified_Events(queryName_LU, queryDecrip_LU, yearlyRecDF,
-                                                                             qcCheckInstance, dmInstance)
+                outFun = qcProtcol_SNPLPORE.qa_a102_Unverified_Events(nobueno2, queryDecrip_LU, yearlyRecDF, qcCheckInstance,
+                                                                      dmInstance)
                 inQuerySel = outFun[0]
                 flagFieldsDic = outFun[1]
             elif queryName_LU == "qa_f112_Incomplete_Weather":
-                outFun = flagFieldsDic = qcProtcol_SNPLPORE.qa_f112_Incomplete_Weather(queryName_LU, queryDecrip_LU, yearlyRecDF,
-                                                                              qcCheckInstance, dmInstance)
+                outFun = flagFieldsDic = qcProtcol_SNPLPORE.qa_f112_Incomplete_Weather(queryDecrip_LU, yearlyRecDF,
+                                                                                       qcCheckInstance, dmInstance)
                 inQuerySel = outFun[0]
                 flagFieldsDic = outFun[1]
             else:
@@ -108,10 +112,12 @@ class qcProtcol_SNPLPORE:
                 logMsg = f"Success Applying QC Flags for  - {queryName_LU}"
                 dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
 
-        except:
+        except Exception as e:
+            logMsg = (f'ERROR - An error occurred in QC_Checks_SNPLPORE - processQuery - for query {queryName_LU}: {e}')
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.error(logMsg, exc_info=True)
             traceback.print_exc(file=sys.stdout)
-        finally:
-            return f'Success - processQuery - {queryName_LU}'
+            exit()
 
 
     def qa_a102_Unverified_Events(queryDecrip_LU, yearlyRecDF, qcCheckInstance, dmInstance):
@@ -130,38 +136,39 @@ class qcProtcol_SNPLPORE:
                                 applied.  Additionally defines the flag to be applied
         """
 
-        #Single Query Check
-        queryName_LU = 'qa_a102_Unverified_Events'
+        try:
+            #Single Query Check
+            queryName_LU = 'qa_a102_Unverified_Events'
 
-        #inQuerySel = f"""SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name, qsel_QA_Control.QCFlag, qsel_QA_Control.QCNotes, tlu_Data_Processing_Level.Label AS DataProcessingLevel, "frm_Data_Entry" AS varObject, "[Event_ID] = '" & [tbl_Events].[Event_ID] & "'" AS VarFilter, "" AS varSubObject, "" AS varSubFilter FROM qsel_QA_Control LEFT JOIN tlu_Data_Processing_Level ON qsel_QA_Control.DataProcessingLevelID = tlu_Data_Processing_Level.DataProcessingLevelID WHERE (((qsel_QA_Control.DataProcessingLevelID)<2 Or (qsel_QA_Control.DataProcessingLevelID) Is Null)) ORDER BY qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name;"""
-        inQuerySel = (f"SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name,"
-                      f" qsel_QA_Control.QCFlag, qsel_QA_Control.QCNotes, tlu_Data_Processing_Level.Label AS"
-                      f" DataProcessingLevel, 'frm_Data_Entry' AS varObject, 'tbl_Events' AS RecTable, 'Event_ID' AS"
-                      f" RecField, qsel_QA_Control.Event_ID AS RecValue FROM qsel_QA_Control LEFT JOIN"
-                      f" tlu_Data_Processing_Level ON qsel_QA_Control.DataProcessingLevelID ="
-                      f" tlu_Data_Processing_Level.DataProcessingLevelID WHERE"
-                      f" (((qsel_QA_Control.DataProcessingLevelID)<2 Or (qsel_QA_Control.DataProcessingLevelID)"
-                      f" Is Null)) ORDER BY qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name;")
+            #inQuerySel = f"""SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name, qsel_QA_Control.QCFlag, qsel_QA_Control.QCNotes, tlu_Data_Processing_Level.Label AS DataProcessingLevel, "frm_Data_Entry" AS varObject, "[Event_ID] = '" & [tbl_Events].[Event_ID] & "'" AS VarFilter, "" AS varSubObject, "" AS varSubFilter FROM qsel_QA_Control LEFT JOIN tlu_Data_Processing_Level ON qsel_QA_Control.DataProcessingLevelID = tlu_Data_Processing_Level.DataProcessingLevelID WHERE (((qsel_QA_Control.DataProcessingLevelID)<2 Or (qsel_QA_Control.DataProcessingLevelID) Is Null)) ORDER BY qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name;"""
+            inQuerySel = (f"SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name,"
+                          f" qsel_QA_Control.QCFlag, qsel_QA_Control.QCNotes, tlu_Data_Processing_Level.Label AS"
+                          f" DataProcessingLevel, 'frm_Data_Entry' AS varObject, 'tbl_Events' AS RecTable, 'Event_ID' AS"
+                          f" RecField, qsel_QA_Control.Event_ID AS RecValue FROM qsel_QA_Control LEFT JOIN"
+                          f" tlu_Data_Processing_Level ON qsel_QA_Control.DataProcessingLevelID ="
+                          f" tlu_Data_Processing_Level.DataProcessingLevelID WHERE"
+                          f" (((qsel_QA_Control.DataProcessingLevelID)<2 Or (qsel_QA_Control.DataProcessingLevelID)"
+                          f" Is Null)) ORDER BY qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name;")
 
-        flagFieldsDic = {'ApplyFlag': ['No']}
+            flagFieldsDic = {'ApplyFlag': ['No']}
 
-        return flagFieldsDic
+            return flagFieldsDic
 
-        # Push Yearly Records to Query back to Backend (i.e. qsel_QA_Control)
-        qc.qcChecks.pushQueryToDB(inQuerySel, queryName_LU, qcCheckInstance, dmInstance)
+            # Push Yearly Records to Query back to Backend (i.e. qsel_QA_Control)
+            qc.qcChecks.pushQueryToDB(inQuerySel, queryName_LU, qcCheckInstance, dmInstance)
 
-        # Define the description for the created query
-        dm.generalDMClass.queryDesc(queryName_LU, queryDecrip_LU, qcCheckInstance)
+            # Define the description for the created query
+            dm.generalDMClass.queryDesc(queryName_LU, queryDecrip_LU, qcCheckInstance)
 
-        return inQuerySel, flagFieldsDic
+        except:
+            traceback.print_exc(file=sys.stdout)
+        finally:
+            return inQuerySel, flagFieldsDic
 
-
-    def qa_f112_Incomplete_Weather(queryName_LU, queryDecrip_LU, yearlyRecDF, qcCheckInstance, dmInstance):
+    def qa_f112_Incomplete_Weather (queryDecrip_LU, yearlyRecDF, qcCheckInstance, dmInstance):
         """
-        Query routine for validation check - qa_a102_Unverified_Events
+        Query routine for validation check - qa_f112_Incomplete_Weather
 
-        :param queryName_LU: Name of query routine being processes this is query name in the 'Query_Name' field in
-         table 'tbl_QCQueries'
         :param queryDecrip_LU: Query description pulled from the 'tbl_QCQueries' table
         :param yearlyRecDF:  Dataframe with the subset of yearly records by Event to be processed
         :param qcCheckInstance: QC Check Instance
@@ -172,30 +179,37 @@ class qcProtcol_SNPLPORE:
                                 applied.  Additionally defines the flag to be applied
         """
 
-        #Single Query Check
-        queryName_LU = 'qa_f112_Incomplete_Weather'
+        try:
+            #Single Query Check
+            queryName_LU = 'qa_f112_Incomplete_Weather'
 
-        inQuerySel = (f"SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name, "
-                      f"tbl_Event_Details.QCFlag AS EventDetailsQCFlag, tbl_Event_Details.QCNotes AS "
-                      f"EventDetailsQCNotes, tbl_Event_Details.Incomplete_Survey, tbl_Event_Details.Wind_Spd, "
-                      f"tbl_Event_Details.Wind_Max, tbl_Event_Details.Wind_Dir, tbl_Event_Details.Air_Temp, "
-                      f"tbl_Event_Details.Rel_Hum, tbl_Event_Details.Cloud_Cover, tbl_Event_Details.Event_Notes, "
-                      f"'frm_Data_Entry' AS varObject, 'tbl_Event_Details' AS RecTable, 'Event_ID' AS RecField,"
-                      f" tbl_Event_Details.Event_ID AS RecValue FROM qsel_QA_Control LEFT JOIN tbl_Event_Details ON"
-                      f" qsel_QA_Control.Event_ID = tbl_Event_Details.Event_ID WHERE"
-                      f" (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Wind_Spd) Is Null))"
-                      f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Wind_Max) Is Null))"
-                      f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Wind_Dir) Is Null))"
-                      f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Air_Temp) Is Null))"
-                      f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Rel_Hum) Is Null))"
-                      f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND "
-                      f" ((tbl_Event_Details.Cloud_Cover) Is Null))"
-                      f" ORDER BY qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name;")
+            inQuerySel = (f"SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name, "
+                          f"tbl_Event_Details.QCFlag AS EventDetailsQCFlag, tbl_Event_Details.QCNotes AS "
+                          f"EventDetailsQCNotes, tbl_Event_Details.Incomplete_Survey, tbl_Event_Details.Wind_Spd, "
+                          f"tbl_Event_Details.Wind_Max, tbl_Event_Details.Wind_Dir, tbl_Event_Details.Air_Temp, "
+                          f"tbl_Event_Details.Rel_Hum, tbl_Event_Details.Cloud_Cover, tbl_Event_Details.Event_Notes, "
+                          f"'frm_Data_Entry' AS varObject, 'tbl_Event_Details' AS RecTable, 'Event_ID' AS RecField,"
+                          f" tbl_Event_Details.Event_ID AS RecValue FROM qsel_QA_Control LEFT JOIN tbl_Event_Details ON"
+                          f" qsel_QA_Control.Event_ID = tbl_Event_Details.Event_ID WHERE"
+                          f" (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Wind_Spd) Is Null))"
+                          f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Wind_Max) Is Null))"
+                          f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Wind_Dir) Is Null))"
+                          f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Air_Temp) Is Null))"
+                          f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND ((tbl_Event_Details.Rel_Hum) Is Null))"
+                          f" OR (((tbl_Event_Details.Incomplete_Survey)=False) AND "
+                          f" ((tbl_Event_Details.Cloud_Cover) Is Null))"
+                          f" ORDER BY qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name;")
 
-        # Define the flag fields in the 'tbl_Event_Details' table, these are the fields to which the flag 'DFO' will be
-        # applied
+            # Define the flag fields in the 'tbl_Event_Details' table, these are the fields to which the flag 'DFO' will be
+            # applied
 
-        flagFieldsDic = {'ApplyFlag': ['Yes']}
+            flagFieldsDic = {'ApplyFlag': ['Yes']}
 
-        return inQuerySel, flagFieldsDic
+        except:
+            traceback.print_exc(file=sys.stdout)
 
+        finally:
+            return inQuerySel, flagFieldsDic
+
+    if __name__ == "__name__":
+        logger.info("Running QC_Checks_SNPLPORE.py")

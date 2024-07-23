ff51b6cf51ed12aef605fadb6423a3722a7b6023
krsherrill <kirk_Sherrill@nps.gov>
2024-07-23 14:03:58 -0600

    Update QC_Checks_SNPLPORE.py
    Added  method 'qa_f142_MoreBandedSNPL_ThanChecked'

--- QC_Checks_SNPLPORE.py
+++ QC_Checks_SNPLPORE.py
@@ -94,11 +94,20 @@ class qcProtcol_SNPLPORE:
                 flagFieldsDic = outFun[1]
 
             elif queryName_LU == "qa_f132_MoreCheckedSNPL_ThanTotal":
-                outFun = qcProtcol_SNPLPORE.qa_f132_MoreCheckedSNPL_ThanTotal(queryDecrip_LU, yearlyRecDF,
-                                                                              qcCheckInstance, dmInstance)
+                outFun = qcProtcol_SNPLPORE.qa_f132_MoreCheckedSNPL_ThanTotal(queryDecrip_LU,
+                                                                                  yearlyRecDF,
+                                                                                  qcCheckInstance,
+                                                                                  dmInstance)
                 inQuerySel = outFun[0]
                 flagFieldsDic = outFun[1]
 
+
+
+            elif queryName_LU == "qa_f142_MoreBandedSNPL_ThanChecked":
+                outFun = qcProtcol_SNPLPORE.qa_f142_MoreBandedSNPL_ThanChecked(queryDecrip_LU, yearlyRecDF,
+                                                                              qcCheckInstance, dmInstance)
+                inQuerySel = outFun[0]
+                flagFieldsDic = outFun[1]
 
             else:
                 logMsg = f'Query - {queryName_LU} - is not defined - existing script'
@@ -297,7 +306,7 @@ class qcProtcol_SNPLPORE:
 
         try:
             # Single Query Check
-            queryName_LU = 'qa_f122_CompleteSurvey_IncompleteSNPL'
+            queryName_LU = 'qa_f132_MoreCheckedSNPL_ThanTotal'
 
             inQuerySel = (f"SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name,"
                           f" tbl_Event_Details.QCFlag AS EventDetailsQCFlag, tbl_Event_Details.QCNotes AS"
@@ -324,7 +333,50 @@ class qcProtcol_SNPLPORE:
             logging.error(logMsg, exc_info=True)
             traceback.print_exc(file=sys.stdout)
             exit()
+    def qa_f142_MoreBandedSNPL_ThanChecked(queryDecrip_LU, yearlyRecDF, qcCheckInstance, dmInstance):
+        """
+        Query routine for validation check - qa_f142_MoreBandedSNPL_ThanChecked. Returns surveys that have more SNPL
+        with bands (SNPL_Banded)  than were checked for bands (SNPL_Checked_Bands).  QC default value is LESPB.
 
+        :param queryDecrip_LU: Query description pulled from the 'tbl_QCQueries' table
+        :param yearlyRecDF:  Dataframe with the subset of yearly records by Event to be processed
+        :param qcCheckInstance: QC Check Instance
+        :param dmInstance: data management instance which will have the logfile name
+
+        :return: inQuerySel: Final query to be pushed back to Access DB
+                flagFieldsDic: Dictionary defining the Flag fields in 'tbl_Event_Details' to which flags will be
+                                applied.  Additionally defines the flag to be applied
+        """
+
+        try:
+            # Single Query Check
+            queryName_LU = 'qa_f142_MoreBandedSNPL_ThanChecked'
+
+            inQuerySel = (f"SELECT qsel_QA_Control.Event_ID, qsel_QA_Control.Start_Date, qsel_QA_Control.Loc_Name,"
+                          f" tbl_Event_Details.QCFlag AS EventDetailsQCFlag, tbl_Event_Details.QCNotes AS"
+                          f" EventDetailsQCNotes, tbl_Event_Details.SNPL_Adults, tbl_Event_Details.SNPL_Hatchlings,"
+                          f" tbl_Event_Details.SNPL_Fledglings, tbl_Event_Details.SNPL_Checked_Bands,"
+                          f" tbl_Event_Details.SNPL_Banded, tbl_Event_Details.Event_Notes, 'frm_Data_Entry' AS"
+                          f" varObject, 'tbl_Event_Details' AS RecTable, 'Event_ID' AS RecField,"
+                          f" tbl_Event_Details.Event_ID AS RecValue FROM qsel_QA_Control INNER JOIN tbl_Event_Details"
+                          f" ON qsel_QA_Control.Event_ID = tbl_Event_Details.Event_ID WHERE"
+                          f" (((tbl_Event_Details.SNPL_Banded)>[SNPL_Checked_Bands])) ORDER BY"
+                          f" qsel_QA_Control.Start_Date DESC;")
+
+            # Define the flag fields in the 'tbl_Event_Details' table, these are the fields to which the flag 'DFO' will be
+            # applied
+
+            flagFieldsDic = {'ApplyFlag': ['Yes']}
+
+            return inQuerySel, flagFieldsDic
+
+        except Exception as e:
+
+            logMsg = (f'ERROR - An error occurred in QC_Checks_SNPLPORE - for query {queryName_LU}: {e}')
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.error(logMsg, exc_info=True)
+            traceback.print_exc(file=sys.stdout)
+            exit()
 
     if __name__ == "__name__":
         logger.info("Running QC_Checks_SNPLPORE.py")
